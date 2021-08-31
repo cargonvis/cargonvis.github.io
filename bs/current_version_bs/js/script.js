0a0a1698bc848817bf8049fd784292d9d9f0dc99
Carlos Gonzalez <cgonv1993@gmail.com>
2021-08-31 19:48:33 +0300

    31.08.21 commit

---
+++ bs/current_version_bs/js/script.js
@@ -0,0 +1,5 @@
+// Welcoming message
+alert("If you are using a smartphone, for a better experience, rotate your phone :)");
+
+// No right click in website
+// document.addEventListener('contextmenu', event => event.preventDefault());
