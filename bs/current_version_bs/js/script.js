1547d132393c791a38b7cb9af2aef27b6d5c4673
Carlos Gonzalez <cgonv1993@gmail.com>
2021-08-31 20:00:28 +0300

    31.08.21 commit

--- bs/current_version_bs/js/script.js
+++ bs/current_version_bs/js/script.js
@@ -1,5 +1,10 @@
 // Welcoming message
-alert("If you are using a smartphone, for a better experience, rotate your phone :)");
+
+var alerted = localStorage.getItem('alerted') || '';
+if (alerted != 'yes') {
+ alert("If you are using a smartphone, for a better experience, rotate your phone :)");
+ localStorage.setItem('alerted','yes');
+}
 
 // No right click in website
 // document.addEventListener('contextmenu', event => event.preventDefault());
