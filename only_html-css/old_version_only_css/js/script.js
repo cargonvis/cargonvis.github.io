9966753c79e68ea65303604fd586c83a9954918d
cargonvis <cgonv1993@gmail.com>
2023-02-07 15:16:42 +0200

    website

--- only_html-css/old_version_only_css/js/script.js
+++
@@ -1,5 +0,0 @@
-// Welcoming message
-// alert("Welcome to Carlos' Portfolio Website!. Please accept the cookies of this website :)");
-
-// No right click in website
-// document.addEventListener('contextmenu', event => event.preventDefault());
